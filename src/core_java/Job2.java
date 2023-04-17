7dc30fe0fc719be284242fdaf14621f235846a8f
evar5594 <evar5594@elpoli.edu.co>
2023-04-17 03:09:18 -0500

    some lines of code added by evar5594

--- src/core_java/Job2.java
+++ src/core_java/Job2.java
@@ -5,6 +5,9 @@ public class Job2 {
 		System.out.println("hello");
 		System.out.println("Git learning");
 
+       System.out.println("Line added at the same file and time by the cloner developer");
+       System.out.println("Line added at the same file and time by the cloner developer");
+
 	}
 
 }
